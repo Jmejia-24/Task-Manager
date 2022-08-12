e280622851f4aca04ad17361f3428cd4ec00db56
Byron Mejia <isitsmt-4118@ucan.edu.ni>
2022-08-12 15:03:21 -0600

    Initial Commit

---
+++ Task Manager/Task_ManagerApp.swift
@@ -0,0 +1,20 @@
+//
+//  Task_ManagerApp.swift
+//  Task Manager
+//
+//  Created by Byron Mejia on 8/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Task_ManagerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
