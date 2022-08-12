1764853bc0e187ad44e4d0ebf3537bff452bec4a
Byron Mejia <isitsmt-4118@ucan.edu.ni>
2022-08-12 16:56:28 -0600

    added data for the new task

---
+++ Task Manager/View/AddNewTask.swift
@@ -0,0 +1,141 @@
+//
+//  AddNewTask.swift
+//  Task Manager
+//
+//  Created by Byron Mejia on 8/12/22.
+//
+
+import SwiftUI
+
+struct AddNewTask: View {
+    @EnvironmentObject var taskViewModel: TaskViewModel
+    @Environment(\.self) var env
+    
+    var body: some View {
+        VStack(spacing: 12) {
+            Text("Edit Task")
+                .font(.title3.bold())
+                .frame(maxWidth: .infinity)
+                .overlay(alignment: .leading) {
+                    Button {
+                        env.dismiss()
+                    } label: {
+                        Image(systemName: "arrow.left")
+                            .font(.title3)
+                            .foregroundColor(.black)
+                    }
+                }
+            
+            VStack(alignment: .leading, spacing: 12) {
+                Text("Task Color")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+                
+                let colors = ["Yellow", "Green", "Blue", "Purple", "Red", "Orange"]
+                
+                HStack(spacing: 15) {
+                    ForEach(colors, id: \.self) { color in
+                        Circle()
+                            .fill(Color(color))
+                            .frame(width: 25, height: 25)
+                            .background {
+                                if taskViewModel.taskColor == color {
+                                    Circle()
+                                        .strokeBorder(.gray)
+                                        .padding(-3)
+                                }
+                            }
+                            .contentShape(Circle())
+                            .onTapGesture {
+                                taskViewModel.taskColor = color
+                            }
+                    }
+                }
+                .padding(.top, 10)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.top, 30)
+            
+            Divider()
+                .padding(.vertical, 10)
+            
+            VStack(alignment: .leading, spacing: 12) {
+                Text("Task DeadLine")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+                
+                Text(taskViewModel.taskDeadline.formatted(date: .abbreviated, time: .omitted) + ", " + taskViewModel.taskDeadline.formatted(date: .omitted, time: .shortened))
+                    .font(.callout)
+                    .fontWeight(.semibold)
+                    .padding(.top, 8)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .overlay(alignment: .bottomTrailing) {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "calendar")
+                        .foregroundColor(.black)
+                }
+            }
+            
+            Divider()
+            
+            VStack(alignment: .leading, spacing: 12) {
+                Text("Task Title")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+                
+                TextField("", text: $taskViewModel.taskTitle)
+                    .frame(maxWidth: .infinity)
+                    .padding(.top, 8)
+            }
+            .padding(.top, 10)
+            
+            Divider()
+            
+            // MARK: Sample Task Types
+            let taskTypes = ["Basic", "Urgent", "Important"]
+            VStack(alignment: .leading, spacing: 12) {
+                Text("Task Type")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+                
+                HStack(spacing: 12) {
+                    ForEach(taskTypes, id: \.self) { type in
+                        Text(type)
+                            .font(.callout)
+                            .padding(.vertical, 8)
+                            .frame(maxWidth: .infinity)
+                            .foregroundColor(taskViewModel.taskType == type ? .white : .black)
+                            .background {
+                                if taskViewModel.taskType == type {
+                                    Capsule()
+                                        .fill(.black)
+                                } else {
+                                    Capsule()
+                                        .strokeBorder(.black)
+                                }
+                            }
+                            .contentShape(Capsule())
+                            .onTapGesture {
+                                withAnimation {
+                                    taskViewModel.taskType = type
+                                }
+                            }
+                    }
+                }
+                .padding(.top, 8)
+            }
+        }
+        .frame(maxHeight: .infinity, alignment: .top)
+        .padding()
+    }
+}
+
+struct AddNewTask_Previews: PreviewProvider {
+    static var previews: some View {
+        AddNewTask()
+            .environmentObject(TaskViewModel())
+    }
+}
--- Task Manager/ViewModel/TaskViewModel.swift
+++ Task Manager/ViewModel/TaskViewModel.swift
@@ -10,4 +10,11 @@ import CoreData
 
 class TaskViewModel: ObservableObject {
     @Published var currentTab: String = "Today"
+    
+    // MARK: New Task Properties
+    @Published var openEditTask: Bool = false
+    @Published var taskTitle: String = ""
+    @Published var taskColor: String = "Yellow"
+    @Published var taskDeadline: Date = Date()
+    @Published var taskType: String = "Basic"
 }
